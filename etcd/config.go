d2636fbee44d5791e9d7f5d56e2d74061f9b0557
biningo <biningo.cn@gmail.com>
2021-07-19 17:25:09 +0800

    feat: Etcd implementation registry center

---
+++ etcd/config.go
@@ -0,0 +1,7 @@
+package etcd
+
+/**
+*@Author icepan
+*@Date 7/19/21 15:24
+*@Describe
+**/
---
+++ etcd/registry.go
@@ -0,0 +1,134 @@
+package etcd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/biningo/eagle/registry"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"time"
+)
+
+/**
+*@Author icepan
+*@Date 7/19/21 14:01
+*@Describe
+**/
+
+// Option is etcd registry option.
+type Option func(o *options)
+
+type options struct {
+	ttl    time.Duration
+	prefix string
+}
+
+// Registry is etcd registry.
+type Registry struct {
+	opts   *options
+	client *clientv3.Client
+	kv     clientv3.KV
+	lease  clientv3.Lease
+}
+
+// New creates etcd registry
+func New(client *clientv3.Client, opts ...Option) *Registry {
+	options := &options{
+		ttl:    time.Second * 15,
+		prefix: "",
+	}
+	for _, o := range opts {
+		o(options)
+	}
+	return &Registry{
+		opts:   options,
+		client: client,
+		kv:     clientv3.NewKV(client),
+	}
+}
+
+// RegisterTTL with register ttl.
+func RegisterTTL(ttl time.Duration) Option {
+	return func(o *options) {
+		o.ttl = ttl
+	}
+}
+
+// Prefix with register prefix
+func Prefix(prefix string) Option {
+	return func(o *options) {
+		o.prefix = prefix
+	}
+}
+
+// Register the registration.
+func (r *Registry) Register(ctx context.Context, service *registry.ServiceInstance) error {
+	key := fmt.Sprintf("%s/%s/%s/%s", r.opts.prefix, service.Namespace, service.Name, service.ID)
+	value, err := json.Marshal(service)
+	if err != nil {
+		return err
+	}
+	if r.lease != nil {
+		r.lease.Close()
+	}
+	r.lease = clientv3.NewLease(r.client)
+	grant, err := r.lease.Grant(ctx, int64(r.opts.ttl))
+	if err != nil {
+		return err
+	}
+	_, err = r.kv.Put(ctx, key, string(value), clientv3.WithLease(grant.ID))
+	if err != nil {
+		return err
+	}
+
+	hb, err := r.client.KeepAlive(ctx, grant.ID)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case _, ok := <-hb:
+				if !ok {
+					return
+				}
+			}
+		}
+	}()
+	return nil
+}
+
+// Deregister the registration.
+func (r *Registry) Deregister(ctx context.Context, service *registry.ServiceInstance) error {
+	defer func() {
+		if r.lease != nil {
+			r.lease.Close()
+		}
+	}()
+	key := fmt.Sprintf("%s/%s/%s/%s", r.opts.prefix, service.Namespace, service.Name, service.ID)
+	_, err := r.client.Delete(ctx, key)
+	return err
+}
+
+// GetService return the service instances according to the service name.
+func (r *Registry) GetService(ctx context.Context, opts ...registry.ServiceOption) ([]*registry.ServiceInstance, error) {
+	svc := &registry.ServiceInstance{}
+	svc.Namespace = "default"
+	for _, o := range opts {
+		o(svc)
+	}
+	key := fmt.Sprintf("%s/%s/%s", r.opts.prefix, svc.Namespace, svc.Name)
+	resp, err := r.kv.Get(ctx, key, clientv3.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+	var instances []*registry.ServiceInstance
+	for _, kv := range resp.Kvs {
+		if err := json.Unmarshal(kv.Value, svc); err != nil {
+			return nil, err
+		}
+		instances = append(instances, svc)
+	}
+	return instances, nil
+}
