95ff2bc94436c584b18ff62ebcf0075051ea510b
biningo <biningo.cn@gmail.com>
2021-07-22 10:09:59 +0800

    feat: server cmd

--- cmd/server.go
+++ cmd/server.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/biningo/eagle/app/router"
 	"github.com/biningo/eagle/app/server"
+	"github.com/biningo/eagle/internal/config"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +18,17 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "run agent server",
 	Run: func(cmd *cobra.Command, args []string) {
-		server.Run(":9090", router.Init())
+		if len(args) == 0 {
+			fmt.Println("[server] unrecognized command")
+			return
+		}
+		switch args[0] {
+		case "run":
+			server.Run(fmt.Sprintf("%s:%s", config.Conf.Host, config.Conf.Port), router.Init())
+		default:
+			fmt.Println("[server] unrecognized command")
+			return
+		}
 	},
 }
 
