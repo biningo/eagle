6ed809f19c15b214c7f3428c76c35b8bf5764a81
biningo <biningo.cn@gmail.com>
2021-07-22 10:04:28 +0800

    feat: agent cmd

---
+++ cmd/agent.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/biningo/eagle/agent"
+	"github.com/spf13/cobra"
+)
+
+/**
+*@Author icepan
+*@Date 7/20/21 10:58
+*@Describe
+**/
+
+var agentCmd = &cobra.Command{
+	Use:   "agent",
+	Short: "run agent",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("[agent] unrecognized command")
+			return
+		}
+		switch args[0] {
+		case "run":
+			agent.Run()
+		default:
+			fmt.Println("[agent] unrecognized command")
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(agentCmd)
+}
