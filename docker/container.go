4f1d2a0af1aad7f20f721b42b1e8f12ad38c3712
biningo <biningo.cn@gmail.com>
2021-07-22 10:11:30 +0800

    feat: some docker utils

--- docker/container.go
+++ docker/container.go
@@ -7,31 +7,56 @@ package docker
 **/
 import (
 	"context"
+	"errors"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
-func init() {
-	SetUp()
-}
-
-var Cli *client.Client
-
-func SetUp() {
-	var err error
-	Cli, err = client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func ListContainerByLabels(ctx context.Context, client *client.Client, labels []string) ([]types.Container, error) {
+func makeLabelFilter(labels []string) filters.Args {
 	args := filters.NewArgs()
 	for _, l := range labels {
 		args.Add("label", l)
 	}
+	return args
+}
+
+func ListContainerByLabels(ctx context.Context, client *client.Client, labels []string) ([]types.Container, error) {
 	return client.ContainerList(ctx, types.ContainerListOptions{
+		Filters: makeLabelFilter(labels),
+	})
+}
+
+func ListContainerByImage(ctx context.Context, client *client.Client, image string) ([]types.Container, error) {
+	containers, err := client.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var result []types.Container
+	for _, c := range containers {
+		if c.Image == image {
+			result = append(result, c)
+		}
+	}
+	return result, nil
+}
+
+func ContainerEvents(ctx context.Context, client *client.Client, labels []string) (<-chan events.Message, <-chan error) {
+	return client.Events(ctx, types.EventsOptions{Filters: makeLabelFilter(labels)})
+}
+
+func GetContainer(ctx context.Context, client *client.Client, id string) (types.Container, error) {
+	args := filters.NewArgs()
+	args.Add("id", id)
+	containers, err := client.ContainerList(ctx, types.ContainerListOptions{
 		Filters: args,
 	})
+	if err != nil {
+		return types.Container{}, err
+	}
+	if len(containers) == 0 {
+		return types.Container{}, errors.New("container does not exist")
+	}
+	return containers[0], err
 }
