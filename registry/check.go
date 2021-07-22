fe9943f249cb9660950731fb4ee35ca961ec1e2a
biningo <biningo.cn@gmail.com>
2021-07-22 10:07:42 +0800

    feat: health check

--- registry/check.go
+++ registry/check.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
@@ -19,19 +20,19 @@ type ServiceCheck struct {
 	Interval                       time.Duration
 	Timeout                        time.Duration
 	DeregisterCriticalServiceAfter time.Duration
-	Address                        string //"127.0.0.1:9090"
+	Address                        string //127.0.0.1:9090
 }
 
 type Checker interface {
 	Ping() bool
 }
 
-type httpChecker struct {
+type HttpChecker struct {
 	ServiceCheck
 	Path string
 }
 
-func (c httpChecker) Ping() bool {
+func (c HttpChecker) Ping() bool {
 	u, _ := url.Parse(fmt.Sprintf("http://%s%s", c.Address, c.Path))
 	resp, err := http.Get(u.String())
 	if err != nil || resp.StatusCode != 200 {
@@ -40,13 +41,35 @@ func (c httpChecker) Ping() bool {
 	return true
 }
 
-type tcpChecker struct {
+type TcpChecker struct {
 	ServiceCheck
 }
 
-func (c tcpChecker) Ping() bool {
+func (c TcpChecker) Ping() bool {
 	if _, err := net.DialTimeout("tcp", c.Address, c.Timeout); err != nil {
 		return false
 	}
 	return true
+}
+
+func Check(svc *ServiceInstance, registry Registrar) {
+	if svc.Check == nil {
+		if err := registry.Deregister(context.Background(), svc); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	tick := time.Tick(svc.Service.Check.Interval)
+	for {
+		select {
+		case <-tick:
+			if ok := registry.HealthCheck(context.Background(), svc); !ok {
+				if err := registry.Deregister(context.Background(), svc); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		default:
+		}
+	}
 }
