625b02db1dcdd4fd291c1b9c08b59094891528b2
biningo <biningo.cn@gmail.com>
2021-07-26 14:33:52 +0800

    refactor: pass the function argument to the etcd client

--- app/router/router.go
+++ app/router/router.go
@@ -6,14 +6,28 @@ package router
 *@Describe
 **/
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/biningo/eagle/internal/config"
+	"github.com/biningo/eagle/utils"
+	"github.com/gin-gonic/gin"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+func initEtcd() *clientv3.Client {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: config.Conf.EtcdConfig.Endpoints,
+	})
+	utils.CheckError(err)
+	return etcdCli
+}
 
 func Init() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, "OK")
 	})
-	InitRegistry(r)
-	InitConfig(r)
+	etcdCli := initEtcd()
+	InitRegistry(r, etcdCli)
+	InitConfig(r, etcdCli)
 	return r
 }
